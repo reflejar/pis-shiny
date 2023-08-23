5f6abd6b722b5712e139167860bb46df2fa20e50
Pano Valdez <marianovaldez92@protonmail.com>
2023-08-23 14:45:18 -0300

    hola saimon

--- apps/testeos/ui.R
+++ apps/testeos/ui.R
@@ -43,29 +43,55 @@ style_hidden <- "
 "
 
 shinyUI(fluidPage(
-  includeCSS("../www/bootstrap.min.css", rel = 'stylesheet'),
+  # includeCSS("../www/bootstrap.min.css", rel = 'stylesheet'),
+  suppressDependencies("bootstrap"),
+  theme="../www/bootstrap.min.css",
   tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css'),
+  tags$head(
+    tags$style(style)
+  ),
 
-  tags$head(tags$style(style)),
-  uiOutput("style"),
-  leafletOutput("map"),
-  tags$footer(
-    fluidRow(
+  tags$nav(
       tags$div(
-          column(
-              tags$div(
-                tags$a(tags$i(class="bi bi-twitter"), href="https://twitter.com/fundacionDER",target="_blank", class="btn mx-3 btn-lg btn-floating"),
-                tags$a(tags$i(class="bi bi-instagram"), href="https://www.instagram.com/democraciaenred/",target="_blank", class="btn mx-3 btn-lg btn-floating"),
-                tags$a(tags$i(class="bi bi-facebook"), href="https://www.facebook.com/democraciaenred",target="_blank", class="btn mx-3 btn-lg btn-floating"),
-                tags$a(tags$i(class="bi bi-linkedin"), href="https://www.linkedin.com/company/democracia-en-red/",target="_blank", class="btn mx-3 btn-lg btn-floating"),
-                class="text-center"
-              ),
-              width=12
-          ),
+        tags$a(
+          tags$img(alt="Isotipo de PIS", height="70px", src="../www/img/PIS_isologo_negro.png")
+        ),
+        tags$nav(
+          tags$a("Volver a PIS",class="btn text-uppercase", href="https://pis.org.ar"),
+          class="navbar-nav"
+        ),
+        class="container"
       ),
-    ),
-    class="text-white position-relative pb-5 container",
-    id="footer",
+      class="text-primary navbar navbar-expand-md navbar-light bg-light fixed-top",
+  ),
+  uiOutput("style"),
+  tags$div(
+    leafletOutput("map"),
+    class="my-5 mx-5 min-vh-100"
+  ),
+  
+  tags$footer(
+      tags$div(
+        fluidRow(
+          tags$div(
+            fluidRow(
+              tags$div(
+                tags$img(alt="Isotipo de PIS", height="70px", src="../www/img/PIS_isologo_negro.png")
+                class="col-lg-2"
+              ),
+              tags$div(
+                "PIS es un proyecto de",
+                tags$a()
+                class="col-lg-10"
+              )
+            )
+            class="col-lg-6"
+          )
+        )
+        class="container"
+      )
+      class="text-white position-relative py-4",
+      id="footer"
   ),
 ))
 
