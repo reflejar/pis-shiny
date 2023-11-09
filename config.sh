34c9c3ee4b0a97bb669e254b3175e2af05dcf777
Pano Valdez <marianovaldez92@protonmail.com>
2023-11-09 17:38:40 -0300

    agrego variables de entorno

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -z "$APP_CONFIG" ]; then
-  echo "La variable de entorno APP_CONFIG no está definida."
+  echo "La variable de entorno $APP_CONFIG no está definida."
   exit 1
 fi
 
-if [ ! -d "$APP_CONFIG" ]; then
-  echo "La carpeta especificada en APP_CONFIG no existe."
+if [ ! -d "/apps/$APP_CONFIG" ]; then
+  echo "La carpeta especificada en $APP_CONFIG no existe."
   exit 1
 fi
 
-cp -r "$APP_CONFIG"/* /srv/shiny-server/
+cp -r /apps/"$APP_CONFIG"/* /srv/shiny-server/
