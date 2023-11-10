7ac5da36decd3f00b10f7b3ee079ba739910c513
Pano Valdez <marianovaldez92@protonmail.com>
2023-11-10 10:46:37 -0300

    layout

--- apps/testeos/ui.R
+++ apps/testeos/ui.R
@@ -45,12 +45,16 @@ style_hidden <- "
 
 shinyUI(fluidPage(
   # includeCSS("bootstrap.min.css", rel = 'stylesheet'),
-  tags$meta(name="viewport", content="width=device-width, initial-scale=1"),
-  suppressDependencies("bootstrap"),
   theme="bootstrap.min.css",
-  tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css'),
-  tags$head(tags$style(style)),
-  tags$link(rel = "stylesheet", href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap"),
+  suppressDependencies("bootstrap"),
+  
+  tags$head(
+    tags$meta(name="viewport", content="width=device-width, initial-scale=1"),
+    tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css'),
+    tags$style(style),
+    tags$link(rel="icon", type="image/x-icon", href = 'favicon.ico'),
+    tags$link(rel = "stylesheet", href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap"),
+  ),
 
 
   # NAVBAR
@@ -60,7 +64,7 @@ shinyUI(fluidPage(
           tags$img(alt="Isotipo de PIS", height="70px", src="img/PIS_isologo_negro.png")
         ),
         tags$nav(
-          tags$a("Volver a PIS",class="btn text-uppercase", href="https://pis.org.ar"),
+          tags$a("Volver a PIS",class="btn text-uppercase poppins", href="https://pis.org.ar"),
           class="navbar-nav"
         ),
         class="container"
@@ -123,6 +127,24 @@ shinyUI(fluidPage(
           class="mt-3"
         ),
         class="col-lg-8 col-md-12"
+      ),
+      class="row"
+    ),
+
+    tags$div(
+      tags$div(
+        tags$h5(tags$strong("METODOLOGÍA & PRODUCTO"), tags$span("V1.0", class="badge bg-primary text-black mx-3"), class="text-white pt-3 space-grotesk"),
+        tags$p("Esta herramienta es un sistema de información geográfica (GIS) para mapear colaborativamente detección de agroquímicos en humanos , animales y ambiente. Se grafican datos anonimizados para georeferenciar el resultado tanto de análisis de orina que detectan trazas de plaguicidas en humanos, como también distintos análisis sobre la presencia de estos en el ambiente. Podemos clasificar a los análisis ambientales los podemos clasificar en 7 subtipos:"),
+        tags$ul(
+          tags$li("Análisis de agua de red."),
+          tags$li("Análisis de agua de pozo."),
+          tags$li("Análisis de agua superficial."),
+          tags$li("Análisis de agua de lluvia.-Análisis de sedimentos."),
+          tags$li("Análisis de suelo."),
+          tags$li("Análisis de material vegetal."),
+        ),
+        tags$p("El mapa posee información proveniente de distintas fuentes. Investigaciones científicas, estudios realizados por comunidades u organizaciones del territorio, muestras de análisis hechos por individuos particulares y resultados de una investigación realizada por P.I.S de más de 200 testeos realizados en las localidades de Lobos, Mar Chiquita, Saladillo, La Matanza y CABA. Cada dato se encuentra mapeado con su resultado, su fuente correspondiente y una pequeña descripción que amplía su información."),
+        class="col-12 text-white mt-5", id="metodologia-testeos"
       ),
       class="row"
     ),
