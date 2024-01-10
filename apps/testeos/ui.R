19affb06b7075b5823abe4254e9faf8b0e810540
Pano Valdez <marianovaldez92@protonmail.com>
2024-01-10 10:28:20 -0300

    agrego cambios de texto

--- apps/testeos/ui.R
+++ apps/testeos/ui.R
@@ -14,7 +14,8 @@ shinyUI(fluidPage(
   
   tags$head(
     tags$meta(name="viewport", content="width=device-width, initial-scale=1"),
-    tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css'),
+    # tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css'),
+    tags$link(rel = 'stylesheet', type = 'text/css', href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'),
     tags$link(rel="stylesheet", href = 'custom.css'),
     tags$link(rel="icon", type="image/x-icon", href = 'favicon.ico'),
     tags$link(rel = "stylesheet", href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap"),
@@ -77,7 +78,7 @@ shinyUI(fluidPage(
           ),
           tags$a(
             tags$b("Sumá tus datos"),
-            href="https://donaronline.org/democracia-en-red/campana-recaudacion-proyecto-pis",
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeB0yNZPLNQxbWD-cJKVa96haz74oWVOGEPOZDCUBHEdHb4gg/viewform?usp=sf_link",
             target="_blank",
             class="btn btn-primary text-black mt-2"
           ),
@@ -101,12 +102,14 @@ shinyUI(fluidPage(
         tags$h5(tags$strong("METODOLOGÍA & PRODUCTO"), tags$span("V1.0", class="badge bg-primary text-black mx-3"), class="text-white pt-3 space-grotesk"),
         tags$p("Esta herramienta es un sistema de información geográfica (GIS) para mapear colaborativamente detección de agroquímicos en humanos , animales y ambiente. Se grafican datos anonimizados para georeferenciar el resultado tanto de análisis de orina que detectan trazas de plaguicidas en humanos, como también distintos análisis sobre la presencia de estos en el ambiente. Podemos clasificar a los análisis ambientales los podemos clasificar en 7 subtipos:"),
         tags$ul(
-          tags$li("Análisis de agua de red."),
-          tags$li("Análisis de agua de pozo."),
-          tags$li("Análisis de agua superficial."),
-          tags$li("Análisis de agua de lluvia.-Análisis de sedimentos."),
-          tags$li("Análisis de suelo."),
-          tags$li("Análisis de material vegetal."),
+          tags$li(tags$i(class="fa fa-faucet"), "Análisis de agua de red."),
+          tags$li(tags$i(class="fa fa-house-flood-water"), "Análisis de agua de pozo."),
+          tags$li(tags$i(class="fa fa-water"),"Análisis de agua superficial."),
+          tags$li(tags$i(class="fa fa-cloud-rain"), "Análisis de agua de lluvia."),
+          tags$li(tags$i(class="fa fa-mountain"), "Análisis de sedimentos."),
+          tags$li(tags$i(class="fa fa-mound"), "Análisis de suelo."),
+          tags$li(tags$i(class="fa fa-leaf"), "Análisis de material vegetal."),
+          tags$li(tags$i(class="fa fa-fish"), "Análisis en peces."),
         ),
         tags$p("El mapa posee información proveniente de distintas fuentes. Investigaciones científicas, estudios realizados por comunidades u organizaciones del territorio, muestras de análisis hechos por individuos particulares y resultados de una investigación realizada por P.I.S de más de 200 testeos realizados en las localidades de Lobos, Mar Chiquita, Saladillo, La Matanza y CABA. Cada dato se encuentra mapeado con su resultado, su fuente correspondiente y una pequeña descripción que amplía su información."),
         class="col-12 text-white mt-5", id="metodologia-testeos"
